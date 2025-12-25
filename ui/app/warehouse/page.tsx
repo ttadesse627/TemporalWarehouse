f1f0c9886473f86942a723489d33c780c7479cd9
ttadesse627 <ttadesse627@gmail.com>
2025-12-25 23:51:35 +0300

    Just refactored to test

--- ui/app/warehouse/page.tsx
+++ ui/app/warehouse/page.tsx
@@ -5,7 +5,7 @@ import ProductList from "../../components/ProductList";
 export default function WarehouseDashboard() {
   return (
     <div className="p-5 text-gray-900">
-      <h1>Temporal Warehouse</h1>
+      <h1>Temporal WarehouseX</h1>
       <ProductList />
     </div>
   );
